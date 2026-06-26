f9682b380580b210af665d54eb97975729af5b84
mohd kaif <mk2818356@gmail.com>
2026-06-26 23:44:58 +0530

    update schema

--- App/packages/db/client.ts
+++ App/packages/db/client.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adpater = new PrismaPg({
+export const adapter = new PrismaPg({
     connectionString : process.env.DATABASE_URL
 })
 
 const globalForPrisma = globalThis as unknown as { prisma : PrismaClient}
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-    adapter : adpater
+    adapter : adapter
 })
 
 if(process.env.NODE_ENV !== "production" ) globalForPrisma.prisma = prisma
